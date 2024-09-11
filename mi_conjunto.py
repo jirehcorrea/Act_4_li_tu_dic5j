a261e5ef7617144294516f589e83ba482f946fb2
jirehcorrea <143743324+jirehcorrea@users.noreply.github.com>
2024-09-11 07:39:35 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("ejemplo de conjuntos set")
+tipodeprotes={"fut","tennis","basket"}
+print(tipodeprotes)
+print("lista elementos del conjunto tipo deportes loop for")
+print(tipodeprotes)
+for correa in tipodeprotes:
+  print(correa)
---
+++ mi_tuplas.py
@@ -0,0 +1,11 @@
+print("ejemplo de tuplas")
+canciones =("te amo", "el noa noa", "amor eterno")
+print(canciones)
+y=list (canciones)
+print(y)
+y[1]="la puerta negra"
+x= tuple(y)
+print(x)
+print("listado de elementos de la tupla canciones ciclo for ")
+for correa in canciones:
+    print(correa)
